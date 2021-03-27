ae0e18fcd9bbb0b53728119738de46fdad1a8a29
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-03-27 11:52:40 -0400

    initial file setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Fragment } from 'react';
+import MainPage from './components/MainPage';
+
+function App() {
+  return (
+    <Fragment>
+    <div className="container">
+      <MainPage />
+    </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+
+const MainPage = () => {
+    return (
+        <Fragment>
+        <div>
+            <h1>Musical Intentions</h1>
+        </div>
+        </Fragment>
+    )
+}
+
+export default MainPage;
