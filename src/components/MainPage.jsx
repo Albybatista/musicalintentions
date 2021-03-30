bc55f03c7f7bababb30ec23b3b3b17b884fd9b76
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-03-30 13:43:48 -0400

    successful lyrics API call, API link is dynamic

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,35 +2,37 @@ import React, { Fragment, useState } from 'react';
 import axios from 'axios';
 
 const MainPage = () => {
-    const [songTitle, setSongTitle] = useState("");
     const [artist, setArtist] = useState("");
+    const [songTitle, setSongTitle] = useState("");
+    const [lyrics, setLyrics] = useState("");
 
-    const getLyrics = async (e) => {
-        e.preventDefault();
-        let artist = "Coldplay";
-        let songTitle = "Adventure of a Lifetime";
+   
+    const getLyrics = async (artist, songTitle) => {
         try {
-            await axios.get(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`).then((response) => {
-                console.log(response.data);
-            })
+            const response = await axios.get(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`);
+            const apiLyrics = response.data.lyrics;
+            setLyrics(apiLyrics);
         } catch(err) {
             console.error(err.message);
         }
     }
+
     return (
         <Fragment>
-        <div>
-            <h1>Musical Intentions</h1>
-            <label>
-                Artist: 
-            </label>
-            <input type="text" value={artist} onChange={(e) => setArtist(e.value.target)}/>
-            <label>
-                Song Title: 
-            </label>
-            <input type="text" value={songTitle} onChange={(e) => setSongTitle(e.value.target)}/>
-                <button onClick={getLyrics}>Search</button>
-        </div>
+            <div>
+                <h1>Musical Intentions</h1>
+                <label>
+                    Artist: 
+                </label>
+                    <input type="text" value={artist} onChange={(e) => setArtist(e.target.value)}/>
+                <label>
+                    Song Title: 
+                </label>
+                    <input type="text" value={songTitle} onChange={(e) => setSongTitle(e.target.value)}/>
+                <button onClick={() => getLyrics(artist, songTitle)}>Search</button>
+                <h2>Lyrics</h2>
+                <p id="lyrics">{lyrics}</p>
+            </div>
         </Fragment>
     )
 }
