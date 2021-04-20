c4cb5a3a56ebfd137eb7b7dcf11494a1aceab388
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-04-20 17:22:53 -0400

    carousel image as background, working css

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -6,12 +6,10 @@ const Carousel = ({ topSongs }) => {
 
     if(topSongs.length > 0){
         return (
-            <div className={styles['carousel-container']}>
-                <div style={{
-                    backgroundImage: `url(${("https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg")})`
+            <div className={styles['carousel-container']} 
+                style={{
+                    backgroundImage: `url(${(topSongs[0].images.background)})`
                 }}>
-                </div>
-                {/* <img src={ topSongs[0].images.background }></img> */}
                 <div className={styles['left-carousel-side']}>
                     <i className="fas fa-chevron-left fa-lg"></i>
                 </div>
