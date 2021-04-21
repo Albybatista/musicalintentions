6b96066412bbcfb7c32f150e9f572d698fca1902
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-04-21 14:05:41 -0400

    fixed css on carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from './Carousel.module.css';
 
 const Carousel = ({ topSongs }) => {
-
-
     if(topSongs.length > 0){
         return (
+            // TODO: for loop to get 3 - 5 images
+            // TODO: onClick for left side div or icon
+            // TODO: onClick for right side div or icon
             <div className={styles['carousel-container']} 
                 style={{
                     backgroundImage: `url(${(topSongs[0].images.background)})`
@@ -13,11 +14,11 @@ const Carousel = ({ topSongs }) => {
                 <div className={styles['left-carousel-side']}>
                     <i className="fas fa-chevron-left fa-lg"></i>
                 </div>
-                <div className={styles['right-carousel-side']}>
-                    <i className="fas fa-chevron-right fa-lg"></i>
-                </div>
                 <div className={styles['carousel-bottom']}>
                     I am the bottom
+                </div>
+                <div className={styles['right-carousel-side']}>
+                    <i className="fas fa-chevron-right fa-lg"></i>
                 </div>
             </div>
         )
