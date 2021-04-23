ef6a44e64fce48b98f2253d20ae12894df84fca6
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-04-23 13:44:42 -0400

    responsive design and icons added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import styles from './About.module.css';
+import Footer from '../Footer/Footer';
 
 const About = () => {
     return (
         <div>
             <div className={styles['about-container']}>
+                <h2>About the Developers</h2>
                 <div className={styles['top']}>
                     <div className={styles['image']}>
                         Picture: Alby Batista
                     </div>
                     <section className={styles['text-section']}>
-                        Text: Alby Batista
+                        <p>Text: Alby Batista</p>
+                            <p className={styles['socials-links']}>
+                                <a href='https://www.linkedin.com/in/albybatista'>
+                                    <i className='fab fa-linkedin fa-lg'></i>
+                                </a>
+                                <a href='https://www.github.com/albybatista'>
+                                    <i className='fab fa-github fa-lg'></i>
+                                </a>
+                                <a href='https://www.albybatista.com'>
+                                    <i className='fas fa-user-circle fa-lg'></i>
+                                </a>
+                            </p>
                     </section>
                 </div>
+                <span className={styles['divider']}>
+                </span>
                 <div className={styles['bottom']}>
                     <section className={styles['text-section']}>
-                        Text: Noa Kim
+                        <p>Text: Noa Kim</p>
+                            <p className={styles['socials-links']}>
+                                <a href='https://www.linkedin.com/in/noakim'>
+                                    <i className='fab fa-linkedin fa-lg'></i>
+                                </a>
+                                <a href='https://www.github.com/noanonoa/'>
+                                    <i className='fab fa-github fa-lg'></i>
+                                </a>
+                                <a href='https://www.noakim.io'>
+                                    <i className='fas fa-user-circle fa-lg'></i>
+                                </a>
+                            </p>
                     </section>
                     <div className={styles['image']}>
                         Picture: Noa Kim
