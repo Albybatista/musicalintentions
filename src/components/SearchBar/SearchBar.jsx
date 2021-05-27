7340ab72f8713a5583cae845b20984dd28010b7a
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-05-27 15:10:06 -0400

    fixed search on about branch

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,7 +7,7 @@ function SearchBar({ userInput, setUserInput, getGenius, searchToggle, handleInp
     return (
       <div className={styles['SearchBar']}>
         <label htmlFor="search">artist or song:</label>
-        <input id="search" type="text" name="search" onChange={(e) => handleInput(e)}/>
+        <input id="search" type="text" name="search" onChange={(e) => setUserInput(e.target.value)} />
         <button onClick={() => getGenius(userInput)}>search</button>
       </div>
     )
